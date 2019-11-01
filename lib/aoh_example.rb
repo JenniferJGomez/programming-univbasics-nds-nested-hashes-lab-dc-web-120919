25136791405681ae3357a4f34bcbe64a0f041217
Jennifer Gomez <jennifergomezbruno@gmail.com>
2019-11-01 08:31:27 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -3,7 +3,7 @@
 DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
 JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
 PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
-KATE_G = {name=>"Kate Gompert", :occupation=>"None"}
+KATE_G = {name: "Kate Gompert", occupation: "None"}
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
@@ -22,6 +22,8 @@ def literal_aoh
     { name:  "Don Gately", occupation: "Live-in Staff" },
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
     { name:  "Pat Monteseian", occupation: "Staff" },
+    { name: "Kate Gompert", occupation: "None" },
+    { name:  "Bruce Green", occupation: "Fan of Mildred" },
     ]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
