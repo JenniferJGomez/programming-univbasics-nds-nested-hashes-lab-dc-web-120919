6152f6f002924b4a74ef83840aafff041531f466
Jennifer Gomez <jennifergomezbruno@gmail.com>
2019-11-01 08:38:51 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -33,6 +33,6 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  aoh_update[1][:name] = "Joelle van Dyne (PGOAT)"
+  aoh[1][:name] = "Joelle van Dyne (PGOAT)"
 aoh_update
 end
